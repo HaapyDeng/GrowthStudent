797b2e1af5ee51d770f50d438fce8eb55dec1540
HaapyDeng <jzhou_deng@163.com>
2018-09-04 20:38:08 +0800

    视频录制

--- app/src/main/java/com/mpl/GrowthStud/Activity/VideoActivity.java
+++ app/src/main/java/com/mpl/GrowthStud/Activity/VideoActivity.java
@@ -1,11 +1,16 @@
 package com.mpl.GrowthStud.Activity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageButton;
@@ -21,11 +26,13 @@ import com.mpl.GrowthStud.Adapter.GridViewAdapter;
 import com.mpl.GrowthStud.Bean.MainConstant;
 import com.mpl.GrowthStud.R;
 import com.mpl.GrowthStud.Tools.PictureSelectorConfig;
+import com.mpl.GrowthStud.Tools.Utils;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-public class VideoActivity extends AppCompatActivity implements View.OnClickListener {
+public class VideoActivity extends Activity implements View.OnClickListener {
     private Context mContext;
     private ImageButton back;
     private TextView tv_title, tv_commit;
@@ -33,7 +40,11 @@ public class VideoActivity extends AppCompatActivity implements View.OnClickList
     private String wenzi;
     private String achieveId;
     private String headTitle;
-    private ImageView iv_button;
+    private ImageView iv_button, iv_video;
+    private ImageButton btn_delete;
+
+    // 文件路径
+    private String path = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,8 +66,30 @@ public class VideoActivity extends AppCompatActivity implements View.OnClickList
 
         et_wenzi = findViewById(R.id.et_wenzi);
 
+        btn_delete = findViewById(R.id.btn_delete);
+        btn_delete.setOnClickListener(this);
+
         iv_button = findViewById(R.id.iv_button);
         iv_button.setOnClickListener(this);
+
+        iv_video = findViewById(R.id.iv_video);
+        iv_video.setOnClickListener(this);
+
+        File filePathFile = new File("/storage/emulated/0/im/video/");
+        if (filePathFile != null && filePathFile.listFiles() != null) {
+            if (filePathFile.listFiles().length > 0) {
+                path = filePathFile.listFiles()[0].getPath();
+                Bitmap bitmap = Utils.createVideoThumbnail(path);
+                BitmapDrawable drawable = new BitmapDrawable(bitmap);
+                drawable.setTileModeXY(Shader.TileMode.REPEAT,
+                        Shader.TileMode.REPEAT);
+                drawable.setDither(true);
+//                btnPlay.setBackgroundDrawable(drawable);
+                iv_button.setVisibility(View.GONE);
+                iv_video.setBackgroundDrawable(drawable);
+                btn_delete.setVisibility(View.VISIBLE);
+            }
+        }
     }
 
     @Override
@@ -75,7 +108,37 @@ public class VideoActivity extends AppCompatActivity implements View.OnClickList
                 doUploadVideo(wenzi);
                 break;
             case R.id.iv_button:
+                // 启动拍摄的Activity
+                Intent intent = new Intent(VideoActivity.this, VideoRecorderActivity.class);
+                VideoActivity.this.startActivityForResult(intent, 200);
                 break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 200:
+                if (resultCode == RESULT_OK) {
+                    // 成功
+                    path = data.getStringExtra("path");
+                    Toast.makeText(VideoActivity.this, "存储路径为:" + path, Toast.LENGTH_SHORT).show();
+                    // 通过路径获取第一帧的缩略图并显示
+                    Bitmap bitmap = Utils.createVideoThumbnail(path);
+                    BitmapDrawable drawable = new BitmapDrawable(bitmap);
+                    drawable.setTileModeXY(Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+                    drawable.setDither(true);
+//                    btnPlay.setBackgroundDrawable(drawable);
+                    iv_button.setVisibility(View.GONE);
+                    iv_video.setBackgroundDrawable(drawable);
+                    btn_delete.setVisibility(View.VISIBLE);
+                } else {
+                    // 失败
+                }
+                break;
+
         }
     }
 
