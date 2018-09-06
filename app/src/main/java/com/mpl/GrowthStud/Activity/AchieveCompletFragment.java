e034b317308b97b85c7b07ee5d6b46ae7999ed8c
HaapyDeng <jzhou_deng@163.com>
2018-09-06 16:56:55 +0800

    文字和图文详情

--- app/src/main/java/com/mpl/GrowthStud/Activity/AchieveCompletFragment.java
+++ app/src/main/java/com/mpl/GrowthStud/Activity/AchieveCompletFragment.java
@@ -1,5 +1,6 @@
 package com.mpl.GrowthStud.Activity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -7,12 +8,15 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
+import com.mpl.GrowthStud.Adapter.AchieveCompletListViewAdapter;
 import com.mpl.GrowthStud.Adapter.AchieveToDoListViewAdapter;
+import com.mpl.GrowthStud.Bean.AchieveCompletItem;
 import com.mpl.GrowthStud.Bean.AchieveToDoItem;
 import com.mpl.GrowthStud.R;
 
@@ -21,14 +25,17 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import cz.msebera.android.httpclient.Header;
 
 import static android.content.Context.MODE_PRIVATE;
 
-public class AchieveCompletFragment extends Fragment {
+public class AchieveCompletFragment extends Fragment implements AdapterView.OnItemClickListener {
 
     private ListView listView;
+    private List<AchieveCompletItem> mDatas;
+    private AchieveCompletListViewAdapter achieveCompletListViewAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,9 +48,16 @@ public class AchieveCompletFragment extends Fragment {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_achieve_complet, container, false);
         listView = root.findViewById(R.id.listview);
+        listView.setOnItemClickListener(this);
         getCpmpletAchieve();
         // Inflate the layout for this fragment
         return root;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getCpmpletAchieve();
     }
 
     private void getCpmpletAchieve() {
@@ -64,9 +78,24 @@ public class AchieveCompletFragment extends Fragment {
                     if (code == 0) {
                         JSONObject data = response.getJSONObject("data");
                         JSONArray list = data.getJSONArray("list");
+                        mDatas = new ArrayList<AchieveCompletItem>();
                         for (int i = 0; i < list.length(); i++) {
-
+                            JSONObject object = list.getJSONObject(i);
+                            String id = object.getString("id");
+                            String name = object.getString("name");
+                            String type = object.getString("type");
+                            String image = object.getString("image");
+                            String category_name = object.getString("category_name");
+                            String label_name = object.getString("label_name");
+                            String task_star = object.getString("task_star");
+                            String status = object.getString("status");
+                            String role = object.getString("role");
+                            String star = object.getString("star");
+                            AchieveCompletItem achieveCompletItem = new AchieveCompletItem(id, name, type, image, category_name, label_name, task_star, status, role, star);
+                            mDatas.add(achieveCompletItem);
                         }
+                        achieveCompletListViewAdapter = new AchieveCompletListViewAdapter(getActivity(), mDatas);
+                        listView.setAdapter(achieveCompletListViewAdapter);
                     } else {
                         Toast.makeText(getActivity(), response.getString("message"), Toast.LENGTH_LONG).show();
                         return;
@@ -100,4 +129,43 @@ public class AchieveCompletFragment extends Fragment {
     }
 
 
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+        if (mDatas.get(position).getType().equals("1")) {
+            Intent intent = new Intent(getActivity(), WenZiInfoActivity.class);
+            Bundle bundle = new Bundle();
+            bundle.putString("achieveid", mDatas.get(position).getId());
+            bundle.putString("headtitle", mDatas.get(position).getName());
+            intent.putExtras(bundle);
+            startActivity(intent);
+        } else if (mDatas.get(position).getType().equals("2")) {
+            Intent intent = new Intent(getActivity(), TuWenInfoActivity.class);
+            Bundle bundle = new Bundle();
+            bundle.putString("achieveid", mDatas.get(position).getId());
+            bundle.putString("headtitle", mDatas.get(position).getName());
+            intent.putExtras(bundle);
+            startActivity(intent);
+        } else if (mDatas.get(position).getType().equals("3")) {
+            Intent intent = new Intent(getActivity(), VideoActivity.class);
+            Bundle bundle = new Bundle();
+            bundle.putString("achieveid", mDatas.get(position).getId());
+            bundle.putString("headtitle", mDatas.get(position).getName());
+            intent.putExtras(bundle);
+            startActivity(intent);
+        } else if (mDatas.get(position).getType().equals("4")) {
+            Intent intent = new Intent(getActivity(), QuestionnaireActivity.class);
+            Bundle bundle = new Bundle();
+            bundle.putString("achieveid", mDatas.get(position).getId());
+            bundle.putString("headtitle", mDatas.get(position).getName());
+            intent.putExtras(bundle);
+            startActivity(intent);
+        } else if (mDatas.get(position).getType().equals("5")) {
+//            Intent intent = new Intent(getActivity(), SyatemAchieveActivity.class);
+//            Bundle bundle = new Bundle();
+//            bundle.putString("achieveid", mDatas.get(position).getId());
+//            bundle.putString("headtitle", mDatas.get(position).getName());
+//            intent.putExtras(bundle);
+//            startActivity(intent);
+        }
+    }
 }
