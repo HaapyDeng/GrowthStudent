8f7433a213c2554c44e5c1bb438b3fd51d4198a8
HaapyDeng <jzhou_deng@163.com>
2018-09-15 17:53:20 +0800

    fix bug

--- app/src/main/java/com/mpl/GrowthStud/Student/Activity/RoadActivity.java
+++ app/src/main/java/com/mpl/GrowthStud/Student/Activity/RoadActivity.java
@@ -2,14 +2,29 @@ package com.mpl.GrowthStud.Student.Activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.mpl.GrowthStud.R;
 
-public class RoadActivity extends Activity {
+public class RoadActivity extends Activity implements View.OnClickListener {
+    private ImageView back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_road);
+        back = findViewById(R.id.back);
+        back.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.back:
+                finish();
+                break;
+        }
+
     }
 }
